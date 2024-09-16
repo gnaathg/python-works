917111397efbf9c02578e1865db0ed405a678ab5
sabarinath <unofficialsabarinath6@gmail.com>
2024-09-16 21:56:54 +0530

    minor update

--- Python/fundamentals/homework1.py
+++ Python/fundamentals/homework1.py
@@ -4,7 +4,7 @@ num2 = 200
 print(f"Before swapping: num1 = {num1} num2 = {num2}")
 
 num1 = num1 * num2 #100*200=20000
-num2 = num1 / num2#100
-num1 = num1 / num2#200
+num2 = num1 / num2 #100
+num1 = num1 / num2 #200
 
 print(f"After swapping: num1 = {num1} num2 = {num2}")
